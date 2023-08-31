7398bb28c3922e0a0ddfd4a8b9a5fd5e110d076a
Pablo Holguin <holguinlopezpablo@gmail.com>
2023-08-31 13:05:19 -0600

    enlaces de añadir y home funcionando

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def new
+        @article = Article.new
+    end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles', to: 'articles#index'
   get '/articles/:id', to: 'articles#show', as: :article
 
